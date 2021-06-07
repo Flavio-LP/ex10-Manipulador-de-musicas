4f4580a4c43fb1a9a1ddc36b1d04cdf63559230d
Flávio <flavio5070@outlook.com>
2021-06-07 10:51:14 -0300

    mudança da rota raiz

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app=express()
 const routes = require('./api/routes')
+const PORT = process.env.PORT || 8080
 
 app.use('/api',routes)
 
-/*app.listen(8080,function(){
+app.listen(PORT,function(){
     console.log("Servidor online")
-})*/
+})
+
+app.get('*', function(req, res){
+    res.redirect('/api/listar')
+})
+
 
